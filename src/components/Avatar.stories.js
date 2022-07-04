c3252a2dd2dacee4de64feffd40775b54048b46d
Alessandro Cunha Lorenz <alessandro.lorenz@compasso.com.br>
2022-07-04 11:23:36 -0300

    feat: distribute step of tutorial

--- src/components/Avatar.stories.js
+++ src/components/Avatar.stories.js
@@ -19,71 +19,81 @@ export default {
       options: [true, false],
     },
   },
+  parameters: {
+    componentSubtitle: 'Displays an image that represents a user or organization',
+  },
 };
 
 const Template = args => <Avatar {...args} />;
 
 export const Controls = Template.bind({});
-/*
- * More on args at:
- * https://storybook.js.org/docs/react/writing-stories/args
- */
-Controls.args = {
-  loading: false,
-  size: 'tiny',
-  username: 'Dominic Nguyen',
-  src: 'https://avatars2.githubusercontent.com/u/263385',
-};
+// /*
+//  * More on args at:
+//  * https://storybook.js.org/docs/react/writing-stories/args
+//  */
+// Controls.args = {
+//   loading: false,
+//   size: 'tiny',
+//   username: 'Dominic Nguyen',
+//   src: 'https://avatars2.githubusercontent.com/u/263385',
+// };
 
-export const Standard = (args) => <Avatar {...args} />;
-Standard.args = {
-  size: "large",
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
-};
+// export const Standard = (args) => <Avatar {...args} />;
+// Standard.args = {
+//   size: "large",
+//   username: "Tom Coleman",
+//   src: "https://avatars2.githubusercontent.com/u/132554",
+// };
 
-export const Sizes = (args) => (
-  <div>
-    <Avatar {...args} size="large" />
-    <Avatar {...args} size="medium" />
-    <Avatar {...args} size="small" />
-    <Avatar {...args} size="tiny" />
-  </div>
-);
-Sizes.args = {
-  username: "Tom Coleman",
-  src: "https://avatars2.githubusercontent.com/u/132554",
-};
+// export const Sizes = (args) => (
+//   <div>
+//     <Avatar {...args} size="large" />
+//     <Avatar {...args} size="medium" />
+//     <Avatar {...args} size="small" />
+//     <Avatar {...args} size="tiny" />
+//   </div>
+// );
+// Sizes.args = {
+//   username: "Tom Coleman",
+//   src: "https://avatars2.githubusercontent.com/u/132554",
+// };
 
-export const Initials = (args) => (
-  <div>
-    <Avatar username="Tom Coleman" />
-    <Avatar username="Dominic Nguyen" />
-    <Avatar username="Kyle Suss" />
-    <Avatar username="Michael Shilman" />
-  </div>
-);
+// Sizes.parameters = {
+//     docs: {
+//       // The story now contains a description
+//       storyDescription: '4 sizes are supported.',
+//     },
+//   };
 
-export const Loading = (args) => (
-    <div>
-      <Avatar {...args} size="large" />
-      <Avatar {...args} size="medium" />
-      <Avatar {...args} size="small" />
-      <Avatar {...args} size="tiny" />
-    </div>
-  );
-  Loading.args = {
-    loading: true,
-  };
+// export const Initials = (args) => (
+//   <div>
+//     <Avatar username="Tom Coleman" />
+//     <Avatar username="Dominic Nguyen" />
+//     <Avatar username="Kyle Suss" />
+//     <Avatar username="Michael Shilman" />
+//   </div>
+// );
 
-export const Large = (args) => (
-  <div>
-    <Avatar loading size="large" />
-    <Avatar size="large" username="Tom Coleman" />
-    <Avatar
-      size="large"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
-  </div>
-);
+// export const Loading = (args) => (
+//     <div>
+//       <Avatar {...args} size="large" />
+//       <Avatar {...args} size="medium" />
+//       <Avatar {...args} size="small" />
+//       <Avatar {...args} size="tiny" />
+//     </div>
+//   );
+//   Loading.args = {
+//     loading: true,
+//   };
+
+// export const Large = (args) => (
+//   <div>
+//     <Avatar loading size="large" />
+//     <Avatar size="large" username="Tom Coleman" />
+//     <Avatar
+//       size="large"
+//       username="Tom Coleman"
+//       src="https://avatars2.githubusercontent.com/u/132554"
+//     />
+//   </div>
+// );
